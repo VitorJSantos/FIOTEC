efcecb7f9ba460df3ff1c1c5b0445df62ba50d76
Vitor Santos <vitor.jsantos@outlook.com>
2023-03-17 11:20:28 -0300

    Delete pagina1.php
    Arquivo não é mais necessário.

--- pagina1.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <form action="glob.php">
-            <label>Escolha a pasta</label>
-        <input type="file" id="filepicker" name="fileList" webkitdirectory multiple />
-        <input type="submit" value="Exibir arquivos">
-        </form>
-    </body>
-</html>
